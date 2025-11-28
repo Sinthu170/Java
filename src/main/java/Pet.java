6d91219c1f1ec6096f00d101ed1fb840addc178d
Sinthu170 <s.spam2908@gmail.com>
2025-11-28 19:23:26 +0100

    Pet ist drin

---
+++ src/main/java/Pet.java
@@ -0,0 +1,23 @@
+public class Pet {
+
+    private final static int id = 0;
+    private final String name;
+    private int age;
+
+    public Pet(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
